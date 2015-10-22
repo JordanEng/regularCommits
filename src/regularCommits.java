7513a4c8ded7c2ad67b6fc56a85a99e7dc1033f8
unknown <ros_jheng@WROSRM31918.seattleschools.org>
2015-10-22 07:55:30 -0700

    Initial commit

---
+++ src/regularCommits.java
@@ -0,0 +1,23 @@
+/**
+ * Created by ros_jheng on 10/21/2015.
+
+ Write a console program that asks for an integer input from 1-100, then prints all of the factors of that number
+ (prime factors only if you're feeling ambitious). Any non-integer input or input outside the given range should
+ print an error message ("Only integer values are allowed" or some such. The program should continue accepting
+ user input until the user enters "quit" (case insensitive), which will exit the program.
+
+ Example input:
+ > 10
+ 1 2 5 10
+ > quit
+
+ */
+import java.util.*;
+
+public class regularCommits {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int input = console.nextInt();
+
+    }
+}
